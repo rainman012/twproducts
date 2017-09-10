d0fbd75d5ce2bbfd2071738dd3229a4f484264a9
rainman012 <tw.hamburg@googlemail.com>
2017-09-10 17:57:28 +0200

    Preisliste erweitert

--- twferien/twferien.php
+++ twferien/twferien.php
@@ -24,21 +24,22 @@ add_action('admin_head', 'twferien_set_stylesheet', PHP_INT_MAX);
 function getFerienPreisListe_func($atts){
     global $wpdb;
 	//echo "atts=".$atts["id"];
-	$results = $wpdb->get_results( $wpdb->prepare("SELECT * FROM ferienuebernachtungspreise WHERE str_to_date(Ende, '%%d.%%m.%%Y') >= CURDATE() AND ID=%d", $atts["id"] ));
-	//var_dump($results);
+	$results = $wpdb->get_results( $wpdb->prepare("SELECT * FROM uebernachtungspreise WHERE str_to_date(Ende, '%%d.%%m.%%Y') >= CURDATE() AND ID=%d", $atts["id"] ));
+	$result = $wpdb->get_row( $wpdb->prepare("SELECT * FROM objektpreise WHERE ID=%d", $atts["id"] ));
+	//var_dump($result);
     $output = "";
     $output .= "<div class='ferien'>";
-		$output .= "<h2>Preisliste</h2>";
-		$output .= "<br>";
+		$output .= "<h2>Preisliste</h2><br>";
+		$output .= "<h4>Mietpreise</h4>";
 		$output .= "<table class='ferien' style='width:auto'>";
 			$output .= "<tr>";
-				$output .= "<th>Beschreibung</th>";
+				$output .= "<th>Saison</th>";
 				$output .= "<th>Beginn</th>";
 				$output .= "<th>Ende</th>";
-				$output .= "<th>Tagespreis</th>";
-				$output .= "<th>Mindestaufenthalt</th>";
-				$output .= "<th>Wochenpreis</th>";
-				$output .= "<th>Folgenachtpreis</th>";
+				$output .= "<th>Tag</th>";
+				$output .= "<th>Woche</th>";
+				$output .= "<th>Folgenacht</th>";
+				$output .= "<th>Mind.</th>";
 			$output .= "</tr>";
 			
 		foreach ($results as $row) {
@@ -47,12 +48,40 @@ function getFerienPreisListe_func($atts){
 				$output .= "<td>".$row->Beginn."</td>";
 				$output .= "<td>".$row->Ende."</td>";
 				$output .= "<td>".$row->Grundpreis." €</td>";
-				$output .= "<td>".$row->Mindestaufenthalt." Tage</td>";
 				$output .= "<td>".$row->Wochenpreis." €</td>";
 				$output .= "<td>".$row->Folgenachtpreis." €</td>";
+				$output .= "<td>".$row->Mindestaufenthalt." Tage</td>";
 			$output .= "</tr>";
 		}
 		
+		$output .= "</table>";
+		$output .= "<h4>Sonstige Bedinungen und Zusatzpreise</h4>";
+		
+		$output .= "<table class='ferien' style='width:auto'>";
+			$output .= "<tr>";
+				$output .= "<th>Beschreibung</th>";
+				$output .= "<th></th>";
+			$output .= "</tr>";
+			$output .= "<tr>";
+				$output .= "<td>Anzahlung</td>";
+				$output .= "<td>".$result->Anzahlung." %</td>";
+			$output .= "</tr>";	
+			$output .= "<tr>";
+				$output .= "<td>Kaution</td>";
+				$output .= "<td>".$result->Kaution." €</td>";
+			$output .= "</tr>";	
+			$output .= "<tr>";
+				$output .= "<td>Endreinigung</td>";
+				$output .= "<td>".$result->Endreinigung." €</td>";
+			$output .= "</tr>";	
+			$output .= "<tr>";
+				$output .= "<td>Wäschepaket (optional)</td>";
+				$output .= "<td>".$result->Waeschepaket." € pro Person</td>";
+			$output .= "</tr>";	
+			$output .= "<tr>";
+				$output .= "<td>Haustier (optional)</td>"; 
+				$output .= "<td>".$result->Haustier." € pro Haustier</td>";
+			$output .= "</tr>";	
 		$output .= "</table>";
 	$output .= "</div>";
 
